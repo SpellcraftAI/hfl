ef65dfd1cbd72599c3401a3ecf2c2680b4ea1e0e
scaredmonad <voidptr9@gmail.com>
2023-10-09 01:23:56 +0300

    feat: skeleton

--- src/__init__.py
+++ src/__init__.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
   try:
     with open(HFLSPEC_PATH, "r") as yaml_file:
       data = yaml.load(yaml_file, Loader=yaml.FullLoader)
-      l = loader.Loader(spec=data)
-      print(l.spec)
+      ld = loader.Loader(data)
+      print(ld.spec)
+      print(ld.metadata.pipeline)
   except FileNotFoundError:
       print(f"The file '{HFLSPEC_PATH}' was not found.")
   except Exception as e:
--- src/loader.py
+++ src/loader.py
@@ -1,3 +1,17 @@
+import metadata
+
 class Loader:
   def __init__(self, spec) -> None:
+    self.metadata = metadata.Metadata()
     self.spec = spec
+    self.write_meta()
+  
+  def write_meta(self):
+    if "task" in self.spec:
+      self.metadata.task = self.spec["task"]
+    if "checkpoint" in self.spec:
+      self.metadata.checkpoint = self.spec["checkpoint"]
+    if "pipeline" in self.spec:
+      self.metadata.pipeline = self.spec["pipeline"]
+    else:
+      self.metadata.pipeline = False
