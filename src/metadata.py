662cb433892b33626a5a08443aac6cb640c17502
scaredmonad <voidptr9@gmail.com>
2023-10-09 02:03:47 +0300

    chore: dump

--- src/metadata.py
+++ src/metadata.py
@@ -1,4 +1,17 @@
+import json
 from transformers import pipeline
+from pygments import highlight
+from pygments.formatters.terminal256 import Terminal256Formatter
+from pygments.lexers.web import JsonLexer
+
+def dict_log(dict):
+  raw_json = json.dumps(dict, indent=4)
+  colorful = highlight(
+      raw_json,
+      lexer=JsonLexer(),
+      formatter=Terminal256Formatter(),
+  )
+  print(colorful)
 
 class Metadata:
   def __init__(self) -> None:
@@ -14,7 +27,7 @@ class Metadata:
   def run_pipeline(self):
     generator = pipeline(self.task, self.checkpoint)
     raw_out = generator(self.sequence)
-    print(raw_out) # ensure --log
+    dict_log(raw_out) # ensure --log
     return raw_out
 
   # in this check, a separate tokenizer/model/head
