1d4899182fb16d819facc5d6c1426ec0016c0cc9
scaredmonad <voidptr9@gmail.com>
2023-10-10 15:59:23 +0300

    feat: SyntheticTransformer

--- src/metadata.py
+++ src/metadata.py
@@ -1,7 +1,14 @@
 from transformers import pipeline, AutoTokenizer
 from utils import dict_log
 
+class SyntheticTransformer:
+  tokenizer_fn: None
+  tokenizer_inputs: None
+  model: None
+
 class Metadata:
+  tx = SyntheticTransformer()
+
   def __init__(self) -> None:
     pass
 
@@ -18,8 +25,6 @@ class Metadata:
     dict_log(raw_out) # ensure --log
     return raw_out
 
-  # in this check, a separate tokenizer/model/head
-  # is specified. the definition is as follows:
   # - custom tokenizer is built or default used
   # - custom model is built from checkpoint or HF default
   #   used (distilbert-base-uncased-finetuned-sst-2-english)
@@ -28,11 +33,17 @@ class Metadata:
     if self.tokenizer is not None and self.tokenizer is not False:
       self.build_tokenizer()
 
+  # @todo: batch inputs (splat inference.sequence)
   def build_tokenizer(self):
     print("Initializing tokenizer...")
     feature_checkp = self.tokenizer["checkpoint"] if "checkpoint" in self.tokenizer else self.checkpoint
     extra_opts = dict(self.tokenizer)
     if "checkpoint" in extra_opts:
       del extra_opts["checkpoint"]
-    print(feature_checkp, extra_opts)
-    pass
+    if "return_tensors" not in extra_opts:
+      extra_opts["return_tensors"] = "pt" # enforce torch
+    tokenizer = AutoTokenizer.from_pretrained(feature_checkp)
+    inputs = tokenizer(self.sequence, **extra_opts)
+    self.tx.tokenizer_fn = tokenizer
+    self.tx.tokenizer_inputs = inputs
+    print(feature_checkp, extra_opts, self.tx.tokenizer_inputs)
